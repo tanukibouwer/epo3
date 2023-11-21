26c23e7d255624a980e733999960c2bfc353914c
Kevin Vermaat <kevinvermaat@icloud.com>
2023-11-21 12:12:31 +0100

    should fix no color on screen

--- vga_stuffs/coloring.vhd
+++ vga_stuffs/coloring.vhd
@@ -20,16 +20,47 @@ use ieee.math_real.all;
 
 entity coloring is
     port (
+        -- global inputs
         clk   : in std_logic;
         reset : in std_logic;
-        
+        -- counter data
+        hcount : in std_logic_vector(9 downto 0);
+        vcount : in std_logic_vector(9 downto 0);
+        -- inputs from memory to determine the x,y locations
+        --RGB data outputs
+        R_data : out std_logic;
+        G_data : out std_logic;
+        B_data : out std_logic
+
     );
 end entity coloring;
 
 architecture behavioural of coloring is
+    signal uns_hcount, uns_vcoutn : unsigned(9 downto 0);
 
 begin
 
-    --bunch of logic to decide what color to draw on what space
+    process (clk)
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                R_data <= '0';
+                G_data <= '0';
+                B_data <= '0';
+            elsif uns_hcount >= 143 and uns_hcount < 783 then
+                R_data <= '1';
+                G_data <= '1';
+                B_data <= '1';
+            elsif uns_vcount >= 34 and uns_vcount < 514 then
+                R_data <= '1';
+                G_data <= '1';
+                B_data <= '1';
+            else
+                R_data <= '0';
+                G_data <= '0';
+                B_data <= '0';
+            end if;
+        end if;
+    end process;
 
 end architecture;

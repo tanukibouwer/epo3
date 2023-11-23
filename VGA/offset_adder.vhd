7e71e48e5fb1771d12c65168fda16243f352d141
Kevin <kevinvermaat@icloud.com>
2023-11-23 21:23:31 +0100

    updated the scaled x-y pos they are now the outer bounds of the character boxes

--- VGA/offset_adder.vhd
+++ VGA/offset_adder.vhd
@@ -20,12 +20,15 @@ use ieee.math_real.all;
 
 entity offset_adder is
     port (
-        clk   : in std_logic;
-        reset : in std_logic;
-        xpos  : in STD_LOGIC_VECTOR(7 downto 0);
-        ypos  : in STD_LOGIC_VECTOR(7 downto 0);
-        xpos_scl : out STD_LOGIC_VECTOR(7 downto 0);
-        ypos_scl  : out STD_LOGIC_VECTOR(7 downto 0));
+        clk       : in std_logic;
+        reset     : in std_logic;
+        xpos      : in std_logic_vector(7 downto 0);
+        ypos      : in std_logic_vector(7 downto 0);
+        xpos_scl1 : out std_logic_vector(7 downto 0);
+        xpos_scl2 : out std_logic_vector(7 downto 0);
+        ypos_scl1 : out std_logic_vector(7 downto 0);
+        ypos_scl2 : out std_logic_vector(7 downto 0)
+    );
 end entity offset_adder;
 
 --shouldnt rtl be behaviour?
@@ -35,15 +38,9 @@ architecture behaviour of offset_adder is
 begin
     xpos_int <= to_integer(unsigned(xpos));
     ypos_int <= to_integer(unsigned(ypos));
-
-
-    xpos_scl <= std_logic_vector(to_unsigned(xpos_int*4, xpos_scl'length));
-    ypos_scl <= std_logic_vector(to_unsigned(ypos_int*4, ypos_scl'length));
-    
-    -- scale with 4
-    -- add x with 108 and y with 0
-    -- scale up the x and y values to the display resolution, see the above
-    -- to be done for all the characters, attacks and platform locations
-    -- offset the x signals to make sure the zero is at the active screen time
+    xpos_scl1 <= std_logic_vector(to_unsigned((xpos_int * 4) - 6, xpos_scl'length));
+    xpos_scl2 <= std_logic_vector(to_unsigned((xpos_int * 4) + 6, xpos_scl'length));
+    ypos_scl1 <= std_logic_vector(to_unsigned((ypos_int * 4) - 6, ypos_scl'length));
+    ypos_scl2 <= std_logic_vector(to_unsigned((ypos_int * 4) + 6, ypos_scl'length));
 
 end architecture;

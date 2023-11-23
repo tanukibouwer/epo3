2bd7706994fada634b1bbb8b26942a44756a161e
Parama Fawwaz <ParamaFawwaz@student.tudelft.nl>
2023-11-23 20:45:23 +0100

    Offset adder 1st implementation

--- VGA/offset_adder.vhd
+++ VGA/offset_adder.vhd
@@ -1,6 +1,6 @@
 --module: offset_adder
 --version: 1.0
---author: Kevin Vermaat
+--author: Kevin Vermaat & Parama Fawwaz
 --------------------------------------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------------------------
 --MODULE DESCRIPTION
@@ -22,21 +22,28 @@ entity offset_adder is
     port (
         clk   : in std_logic;
         reset : in std_logic;
-        -- inputs from memory
-        -- outputs to coloring module
-    );
+        xpos  : in STD_LOGIC_VECTOR(7 downto 0);
+        ypos  : in STD_LOGIC_VECTOR(7 downto 0);
+        xpos_scl : out STD_LOGIC_VECTOR(7 downto 0);
+        ypos_scl  : out STD_LOGIC_VECTOR(7 downto 0));
 end entity offset_adder;
 
-architecture rtl of offset_adder is
+--shouldnt rtl be behaviour?
+architecture behaviour of offset_adder is
+    signal xpos_int, ypos_int : integer;
 
 begin
+    xpos_int <= to_integer(unsigned(xpos));
+    ypos_int <= to_integer(unsigned(ypos));
 
 
+    xpos_scl <= std_logic_vector(to_unsigned(xpos_int*4, xpos_scl'length));
+    ypos_scl <= std_logic_vector(to_unsigned(ypos_int*4, ypos_scl'length));
+    
     -- scale with 4
     -- add x with 108 and y with 0
     -- scale up the x and y values to the display resolution, see the above
     -- to be done for all the characters, attacks and platform locations
-
     -- offset the x signals to make sure the zero is at the active screen time
 
 end architecture;

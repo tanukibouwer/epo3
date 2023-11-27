959f037b2cbb7e263de221d385af786f8fe97116
Kevin <kevinvermaat@icloud.com>
2023-11-27 12:35:08 +0100

    hopefully fixed the offset adder also fixed some syntax errors and formatting things

--- VGA/char_offset_adder.vhd
+++ VGA/char_offset_adder.vhd
@@ -20,29 +20,42 @@ use ieee.math_real.all;
 
 entity char_offset_adder is
     port (
-        xpos      : in std_logic_vector(7 downto 0);
-        ypos      : in std_logic_vector(7 downto 0);
-        xpos_scl1 : out std_logic_vector(7 downto 0);
-        xpos_scl2 : out std_logic_vector(7 downto 0);
-        ypos_scl1 : out std_logic_vector(7 downto 0);
-        ypos_scl2 : out std_logic_vector(7 downto 0)
+        xpos : in std_logic_vector(7 downto 0);
+        ypos : in std_logic_vector(7 downto 0);
+        -- xsize     : in std_logic_vector(3 downto 0);
+        -- ysize     : in std_logic_vector(3 downto 0);
+        xpos_scl1 : out std_logic_vector(10 downto 0);
+        xpos_scl2 : out std_logic_vector(10 downto 0);
+        ypos_scl1 : out std_logic_vector(10 downto 0);
+        ypos_scl2 : out std_logic_vector(10 downto 0)
     );
 end entity char_offset_adder;
 
 architecture behaviour of char_offset_adder is
-    signal xpos_int, ypos_int : unsigned(7 downto 0);
+    -- signal xpos_int, ypos_int : unsigned(7 downto 0);
+    signal xpos_int                                   : integer; -- range 0 to 800;
+    signal ypos_int                                   : integer; -- range 0 to 525;
+    signal xpos_int1, xpos_int2, ypos_int1, ypos_int2 : integer;
 
 begin
-    process (xpos, ypos)
-    begin
-        xpos_scl1 <= std_logic_vector((xpos_int * 4) - (4 * 4) + 108);
-        xpos_scl2 <= std_logic_vector((xpos_int * 4) + (4 * 4) + 108);
-        ypos_scl1 <= std_logic_vector((ypos_int * 4) - (4 * 4));
-        ypos_scl2 <= std_logic_vector((ypos_int * 4) + (4 * 4));
+    -- process (xpos, ypos)
+    -- begin
+    xpos_int1 <= ((xpos_int * 4) - (4 * 4) + 108);
+    xpos_int2 <= ((xpos_int * 4) + (4 * 4) + 108);
+    ypos_int1 <= ((ypos_int * 4) - (4 * 4));
+    ypos_int2 <= ((ypos_int * 4) + (4 * 4));
+    xpos_scl1 <= std_logic_vector(to_unsigned(xpos_int1, xpos_scl1'length));
+    xpos_scl2 <= std_logic_vector(to_unsigned(xpos_int2, xpos_scl1'length));
+    ypos_scl1 <= std_logic_vector(to_unsigned(ypos_int1, xpos_scl1'length));
+    ypos_scl2 <= std_logic_vector(to_unsigned(ypos_int2, xpos_scl1'length));
+    -- xpos_scl1 <= std_logic_vector(to_unsigned((xpos_int * 4) - (4 * 4) + 108), xpos_scl1'length);
+    -- xpos_scl2 <= std_logic_vector(to_unsigned((xpos_int * 4) + (4 * 4) + 108), xpos_scl1'length);
+    -- ypos_scl1 <= std_logic_vector(to_unsigned((ypos_int * 4) - (4 * 4)), xpos_scl1'length);
+    -- ypos_scl2 <= std_logic_vector(to_unsigned((ypos_int * 4) + (4 * 4)), xpos_scl1'length);
 
-    end process;
+    -- end process;
 
-    xpos_int <= unsigned(xpos);
-    ypos_int <= unsigned(ypos);
+    xpos_int <= to_integer(unsigned(xpos));
+    ypos_int <= to_integer(unsigned(ypos));
 
 end architecture;

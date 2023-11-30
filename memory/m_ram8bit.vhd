17b4bf62b48067a8575a04c3bdd649d9c30837f7
Kevin <kevinvermaat@icloud.com>
2023-11-30 21:01:33 +0100

    just a try

--- memory/m_ram8bit.vhd
+++ memory/m_ram8bit.vhd
@@ -20,7 +20,7 @@ begin
 	begin
 		if (rising_edge (clk)) and write = '1' then
 			if (reset = '1') then
-				mem(0) <= "00000000";
+				mem(0) <= std_logic_vector(to_unsigned(50, mem(0)'length));
 			else
 				mem(0) <= data_in;
 			end if;

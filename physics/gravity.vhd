54dd21933a9184e06a45c019fb37b53d90abce88
tanukibouwer <amara@qudical.nl>
2023-12-01 20:23:11 +0100

    Add gravity submodule

---
+++ physics/gravity.vhd
@@ -0,0 +1,24 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+entity gravity is
+	port(	velocity_in 	: in std_logic_vector (8 downto 0);
+			velocity_out	: out std_logic_vector (8 downto 0));
+end gravity;
+
+architecture behaviour of gravity is
+
+	constant gravity_constant : std_logic_vector (8 downto 0)	:= "000000101";
+
+begin
+
+	velocity_out <= std_logic_vector(signed(velocity_in) + signed(gravity_constant));
+
+end architecture behaviour;
+
+configuration gravity_behaviour_cfg of gravity is
+	for behaviour
+	end for;
+end gravity_behaviour_cfg;
+
---
+++ physics/gravity_tb.vhd
@@ -0,0 +1,34 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+entity gravity_tb is
+end gravity_tb;
+
+architecture structural of gravity_tb is
+
+	component gravity is
+		port(	velocity_in		: in std_logic_vector (8 downto 0);
+			velocity_out		: out std_logic_vector (8 downto 0));
+	end component gravity;
+
+	signal velocity_in, velocity_out : std_logic_vector (8 downto 0);
+
+begin
+
+
+	grav: gravity port map (velocity_in, velocity_out);
+
+	velocity_in <=	"000100000" after 0 ns;
+
+end architecture structural;
+
+configuration gravity_tb_structural_cfg of gravity_tb is
+   for structural
+      for all: gravity use configuration work.gravity_behaviour_cfg;
+      end for;
+   end for;
+end gravity_tb_structural_cfg;
+
+
+

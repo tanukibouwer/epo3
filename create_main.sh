6a2370887cfa68b7b2ca3d0e2746c03efe4b95c9
tanukibouwer <amara@qudical.nl>
2023-12-15 15:31:15 +0100

    Remove testbenches from main project.

--- create_main.sh
+++ create_main.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+cd `dirname $0`
 sh link.sh main_proj "../../main/* ../../physics/* ../../memory/* ../../input/* ../../attack/* ../../VGA/*"
+cd `dirname $0`
+cd main_proj/VHDL
+ls -1 | grep "_tb" | xargs rm -f

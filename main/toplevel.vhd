bf0fc0d1215c93ad042e084520ae30bb648a4909
Anthony Chen <A.Z.Chen@student.tudelft.nl>
2023-12-19 10:52:25 +0100

    update toplevel

--- main/toplevel.vhd
+++ main/toplevel.vhd
@@ -105,6 +105,8 @@ architecture structural of chip_toplevel is
             char1_y : in std_logic_vector(7 downto 0); --! character 1 y-location
             char2_x : in std_logic_vector(7 downto 0); --! character 2 x-location
             char2_y : in std_logic_vector(7 downto 0); --! character 2 y-location
+            percentage_p1 : in std_logic_vector(7 downto 0);
+            percentage_p2 : in std_logic_vector(7 downto 0);
             -- outputs to screen (and other components)
             -- vcount : out std_logic_vector(9 downto 0);
             Vsync  : out std_logic; --! sync signals -> active low
@@ -115,101 +117,36 @@ architecture structural of chip_toplevel is
     end component graphics_card;
 
     component memory is
-        port (
-            --charhp  : out std_logic_vector(9 downto 0); -- initial knockback percentage so NOT character health!
-            --chardc  : out std_logic_vector(7 downto 0); -- initial character death count, may not be necessary
-            --char1sx : out std_logic_vector(7 downto 0); -- char(acter)1 starting position x
-            --char1sy : out std_logic_vector(7 downto 0); -- char1 starting position y
-            --char2sx : out std_logic_vector(7 downto 0); -- char2 starting position x
-            --char2sy : out std_logic_vector(7 downto 0); -- char2 starting position y
-            --char1vx : out std_logic_vector(8 downto 0); -- char1 starting velocity x
-            --char1vy : out std_logic_vector(8 downto 0); -- char1 starting velocity y
-            --char2vx : out std_logic_vector(8 downto 0); -- char2 starting velocity x
-            --char2vy : out std_logic_vector(8 downto 0); -- char2 starting velocity y
-            --chardx  : out std_logic_vector(3 downto 0); -- char size x (from center to edge)
-            --chardy  : out std_logic_vector(3 downto 0); -- char size y (from center to edge)
-            --att1dx  : out std_logic_vector(3 downto 0); -- attack1 size x (from center to edge)
-            --att1dy  : out std_logic_vector(3 downto 0); -- attack1 size y (from center to edge)
-            --att2dx  : out std_logic_vector(3 downto 0); -- attack2 size x (from center to edge)
-            --att2dy  : out std_logic_vector(3 downto 0); -- attack2 size y (from center to edge)
-            --att1dm  : out std_logic_vector(3 downto 0); -- attack1 damage
-            --att2dm  : out std_logic_vector(3 downto 0); -- attack2 damage
-            -- att1kb		: out std_logic_vector(5 downto 0); -- attack1 knockback, currently unused
-            -- att2kb		: out std_logic_vector(5 downto 0); -- attack2 knockback, currently unused
-            --plat1x  : out std_logic_vector(7 downto 0); -- plat(form)1 x position
-            --plat1y  : out std_logic_vector(7 downto 0); -- plat1 y position
-            --plat2x  : out std_logic_vector(7 downto 0); -- plat2 x position
-            --plat2y  : out std_logic_vector(7 downto 0); -- plat2 y position
-            --plat3x  : out std_logic_vector(7 downto 0); -- plat3 x position
-            --plat3y  : out std_logic_vector(7 downto 0); -- plat3 y position
-            --plat4x  : out std_logic_vector(7 downto 0); -- plat4 x position
-            --plat4y  : out std_logic_vector(7 downto 0); -- plat4 y position
-            --plat1dx : out std_logic_vector(7 downto 0); -- plat1 x size (center to edge)
-            --plat1dy : out std_logic_vector(7 downto 0); -- plat1 y size (center to edge)
-            --plat2dx : out std_logic_vector(7 downto 0); -- plat2 x size (center to edge)
-            --plat2dy : out std_logic_vector(7 downto 0); -- plat2 y size (center to edge)
-            --plat3dx : out std_logic_vector(7 downto 0); -- plat3 x size (center to edge)
-            --plat3dy : out std_logic_vector(7 downto 0); -- plat3 y size (center to edge)
-            --plat4dx : out std_logic_vector(7 downto 0); -- plat4 x size (center to edge)
-            --plat4dy : out std_logic_vector(7 downto 0); -- plat4 y size (center to edge)
-            --kilznx1 : out std_logic_vector(7 downto 0); -- killzone location (left side)
-            --kilznx2 : out std_logic_vector(7 downto 0); -- killzone location (right side)
-            --kilzny1 : out std_logic_vector(7 downto 0); -- killzone location (top side)
-            -- numbers commented out for now, will be linked to vga module eventually
-            -- num01		: out std_logic_vector(4 downto 0);
-            -- num02		: out std_logic_vector(4 downto 0);
-            -- num03		: out std_logic_vector(4 downto 0);
-            -- num04		: out std_logic_vector(4 downto 0);
-            -- num05		: out std_logic_vector(4 downto 0);
-            -- num11		: out std_logic_vector(4 downto 0);
-            -- num12		: out std_logic_vector(4 downto 0);
-            -- num23		: out std_logic_vector(4 downto 0);
-            -- num24		: out std_logic_vector(4 downto 0);
-            -- num25		: out std_logic_vector(4 downto 0);
-            -- num26		: out std_logic_vector(4 downto 0);
-            -- num27		: out std_logic_vector(4 downto 0);
-            -- num41		: out std_logic_vector(4 downto 0);
-            -- num42		: out std_logic_vector(4 downto 0);
-            -- num43		: out std_logic_vector(4 downto 0);
-            -- num53		: out std_logic_vector(4 downto 0);
-            -- num74		: out std_logic_vector(4 downto 0);
-            -- num94		: out std_logic_vector(4 downto 0);
-            -- num97		: out std_logic_vector(4 downto 0);
-            clk   : in std_logic;
-            reset : in std_logic;
-            vsync : in std_logic;
-            data_in4b1   : in std_logic_vector(3 downto 0); -- death count p1
-            data_in4b2   : in std_logic_vector(3 downto 0); -- death count p2
-            data_out4b1  : out std_logic_vector(3 downto 0); -- death count p1
-            data_out4b2  : out std_logic_vector(3 downto 0); -- death count p2
-            data_inhp1  : in std_logic_vector(7 downto 0); --knockback percentage p1
-            data_inhp2  : in std_logic_vector(7 downto 0); --knockback percentage p2
-            data_outhp1 : out std_logic_vector(7 downto 0); --knockback percentage p1
-            data_outhp2 : out std_logic_vector(7 downto 0); --knockback percentage p2
-            data_in8b1 : in std_logic_vector(7 downto 0); -- x pos p1
-            data_in8b2 : in std_logic_vector(7 downto 0); -- y pos p1
-            data_in8b3 : in std_logic_vector(7 downto 0); -- x pos p2
-            data_in8b4 : in std_logic_vector(7 downto 0); -- y pos p2
-            -- data_in8b5		: in std_logic_vector(7 downto 0); -- x pos attack p1
-            -- data_in8b6		: in std_logic_vector(7 downto 0); -- y pos attack p1
-            -- data_in8b7		: in std_logic_vector(7 downto 0); -- x pos attack p2
-            -- data_in8b8		: in std_logic_vector(7 downto 0); -- y pos attack p2
-            data_out8b1 : out std_logic_vector(7 downto 0); -- x pos p1
-            data_out8b2 : out std_logic_vector(7 downto 0); -- y pos p1
-            data_out8b3 : out std_logic_vector(7 downto 0); -- x pos p2
-            data_out8b4 : out std_logic_vector(7 downto 0); -- y pos p2
-            -- data_out8b5		: out std_logic_vector(7 downto 0); -- x pos attack p1
-            -- data_out8b6		: out std_logic_vector(7 downto 0); -- y pos attack p1
-            -- data_out8b7		: out std_logic_vector(7 downto 0); -- x pos attack p2
-            -- data_out8b8		: out std_logic_vector(7 downto 0); -- y pos attack p2
-            data_in9b1  : in std_logic_vector(8 downto 0); -- x vel p1
-            data_in9b2  : in std_logic_vector(8 downto 0); -- y vel p1
-            data_in9b3  : in std_logic_vector(8 downto 0); -- x vel p2
-            data_in9b4  : in std_logic_vector(8 downto 0); -- y vel p2
-            data_out9b1 : out std_logic_vector(8 downto 0); -- x vel p1
-            data_out9b2 : out std_logic_vector(8 downto 0); -- y vel p1
-            data_out9b3 : out std_logic_vector(8 downto 0); -- x vel p2
-            data_out9b4 : out std_logic_vector(8 downto 0)); -- y vel p2
+        port(
+            clk		: in std_logic;
+            reset		: in std_logic;
+            resetp1		: in std_logic;
+            resetp2		: in std_logic;
+            vsync	 	: in std_logic;
+            data_in4b1	: in std_logic_vector(3 downto 0);
+            data_in4b2	: in std_logic_vector(3 downto 0);
+            data_out4b1	: out std_logic_vector(3 downto 0);
+            data_out4b2	: out std_logic_vector(3 downto 0);
+            data_inhp1	: in std_logic_vector(7 downto 0);
+            data_inhp2	: in std_logic_vector(7 downto 0);
+            data_outhp1	: out std_logic_vector(7 downto 0);
+            data_outhp2	: out std_logic_vector(7 downto 0);
+            data_in8b1 		: in std_logic_vector(7 downto 0);
+            data_in8b2		: in std_logic_vector(7 downto 0);
+            data_in8b3		: in std_logic_vector(7 downto 0);
+            data_in8b4		: in std_logic_vector(7 downto 0);
+            data_out8b1		: out std_logic_vector(7 downto 0);
+            data_out8b2		: out std_logic_vector(7 downto 0);
+            data_out8b3		: out std_logic_vector(7 downto 0);
+            data_out8b4		: out std_logic_vector(7 downto 0);
+            data_in9b1		: in std_logic_vector(8 downto 0);
+            data_in9b2		: in std_logic_vector(8 downto 0);
+            data_in9b3		: in std_logic_vector(8 downto 0);
+            data_in9b4		: in std_logic_vector(8 downto 0);
+            data_out9b1		: out std_logic_vector(8 downto 0);
+            data_out9b2		: out std_logic_vector(8 downto 0);
+            data_out9b3		: out std_logic_vector(8 downto 0);
+            data_out9b4		: out std_logic_vector(8 downto 0));
     end component memory;
 begin
 
@@ -217,6 +154,9 @@ begin
         clk   => clk,
         reset => reset,
         vsync => vsyncintern,
+
+        resetp1 => '0',
+        resetp2 => '0',
 
 		-- death count and percentage
 		data_in4b1 => char1dcin,
@@ -258,6 +198,10 @@ begin
         char1_y => char1posy,
         char2_x => char2posx,
         char2_y => char2posy,
+
+        percentage_p1 => char1perc,
+        percentage_p2 => char2perc,
+
 
         Vsync  => vsyncintern,
         Hsync  => Hsync,

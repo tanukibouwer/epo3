93a6adf33d59ae199d6b51ba8d816e7129c52da4
Kevin Vermaat <kevinvermaat@icloud.com>
2023-12-22 13:29:50 +0100

    forgot the end for;

--- input/input_toplevel_structural_cfg.vhd
+++ input/input_toplevel_structural_cfg.vhd
@@ -1,9 +1,14 @@
 configuration input_toplevel_structural_cfg of input_toplevel is
 	for structural
 		for all: input_driver use configuration work.input_driver_behavioural_cfg;
+end for;
 		for all: input_buffer use configuration work.input_buffer_behavioural_cfg;
-		for all: input_deserialiser use configuration work.input_deserialiser_behavioural_cfg;
+end for;
+		for all: input_deserializer use configuration work.input_deserializer_behavioural_cfg;
+end for;
 		for all: input_register use configuration work.input_register_behavioural_cfg;
+end for;
 		for all: input_jump use configuration work.input_jump_behavioural_cfg;
+end for;
 	end for;
 end configuration input_toplevel_structural_cfg;

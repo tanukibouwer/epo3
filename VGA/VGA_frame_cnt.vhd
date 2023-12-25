e5fd911daa410ed5a928ed69663505703a39722d
Kevin <kevinvermaat@icloud.com>
2023-12-26 00:19:24 +0100

    rename for a more clear name this counter actually counts how many frames have passed

--- VGA/VGA_frame_cnt.vhd
+++ VGA/VGA_frame_cnt.vhd
@@ -1,10 +1,10 @@
---module: Vsync counter
+--module: frame_cnt
 --version: 1.0
 --author: Parama Fawwaz & Kevin Vermaat
 --------------------------------------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------------------------
 --MODULE DESCRIPTION
---
+-- this module counts how many frames have passed to keep track of time
 --
 --
 --------------------------------------------------------------------------------------------------------------------------------
@@ -15,36 +15,37 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.math_real.all;
 
-entity vsync_cnt is
+entity frame_cnt is
     port (
         clk : in std_logic;
         reset : in std_logic;
         vcount : in std_logic_vector(9 downto 0);
-        count : out std_logic
+        hcount : in std_logic_vector(9 downto 0);
+        count : out std_logic_vector(4 downto 0)
     );
 end entity;
 
-architecture behavioural of vsync_cnt is
+architecture behavioural of frame_cnt is
     signal cur_count, new_count : unsigned(4 downto 0);
 begin
 
-    process (reset, vcount, new_count, cur_count) --storage of the count
+    process (clk) --storage of the count --> MAKE SURE THIS IS A REGISTER!
     begin
-        if (reset = '1' and unsigned(vcount) /= 500) then
-            cur_count <= (others => '0');
-        elsif (unsigned(vcount) = 500 and reset = '0') then -- add a count before the vertical frame has ran out (at 796)
-            cur_count <= new_count;
-        else
-            cur_count <= cur_count;
+        if rising_edge(clk) then
+            if reset = '1' then
+                cur_count <= (others => '0');
+            elsif (unsigned(hcount) = 790 and unsigned(vcount) = 500) then -- add a count whenever the vertical frame is close to finishing, only once per frame
+                cur_count <= new_count;
+            end if;
         end if;
     end process;
 
-    process (cur_count) 
+    process (cur_count) --count on clock/input
     begin
         new_count <= cur_count + 1;
     end process;
 
-    count <= std_logic_vector(cur_count)(4);
+    count <= std_logic_vector(cur_count);
     
 end architecture;
 

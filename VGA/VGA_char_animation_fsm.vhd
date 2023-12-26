3b32e2096f374e5199ab5f3ef5093648d1518cc2
Kevin <kevinvermaat@icloud.com>
2023-12-26 01:06:28 +0100

    fixed the fsm and clk generator also set an arbitrary speed, this one seems correct

--- VGA/VGA_char_animation_fsm.vhd
+++ VGA/VGA_char_animation_fsm.vhd
@@ -11,50 +11,75 @@
 --! 
 --------------------------------------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------------------------
-library IEEE;
-use IEEE.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
 
 entity char_animation_fsm is
     port (
-        clk   : in std_logic;
-        reset : in std_logic;
-        -- animation_clk : in std_logic_vector(5 downto 0);
-        vcount : in std_logic_vector(9 downto 0);
-        numstate: out std_logic_vector(6 downto 0);
+        clk    : in std_logic;
+        reset  : in std_logic;
 
+        -- global frame counters
+        vcount : in std_logic_vector(9 downto 0); -- vertical frame counter
+        hcount : in std_logic_vector(9 downto 0); -- horizontal line counter
+
+        -- controller input signal
         controller_in : in std_logic_vector(7 downto 0); -- bit 0 = left, bit 1 = right, bit 2 = up, bit 3 = down
-        sprite : out std_logic_vector(1 downto 0)
+
+        -- sprite output value
+        sprite        : out std_logic_vector(1 downto 0)  
     );
 end char_animation_fsm;
 architecture behaviour of char_animation_fsm is
 
-    component vsync_cnt is
+    component animation_clk_gen is
+        port (
+            clk       : in std_logic;
+            reset     : in std_logic;
+            count     : in std_logic_vector (4 downto 0);
+            cnt_reset : out std_logic -- this also functions as the animation clock
+        );
+    end component;
+
+    component frame_cnt is
         port (
             clk : in std_logic;
             reset : in std_logic;
             vcount : in std_logic_vector(9 downto 0);
-            count : out std_logic
+            hcount : in std_logic_vector(9 downto 0);
+            count : out std_logic_vector(4 downto 0)
         );
     end component;
 
-    signal animation_clk : std_logic;
-    signal cnt_reset : std_logic;
-
-
+    signal cnt_reset     : std_logic;
+    signal sprite_clk : std_logic;
+    signal frame_count : std_logic_vector(4 downto 0);
     type sprite_state is (
         idle, duck, run_frame1, run_frame2
-        );
+    );
     signal state, new_state : sprite_state;
 
 begin
 
-    cnt: vsync_cnt port map (
+    clk_gen : animation_clk_gen port map (
         clk => clk,
-        reset => cnt_reset,
-        vcount => vcount,
-        count => animation_clk
+        reset => reset,
+        count => frame_count,
+        cnt_reset => cnt_reset
     );
-    process (clk)
+
+    cnt : frame_cnt port map(
+        clk    => clk,
+        reset  => cnt_reset,
+        vcount => vcount,
+        hcount => hcount,
+        count  => frame_count
+    );
+
+    process (clk) -- state register -> ONLY REGISTER
     begin
         if rising_edge(clk) then
             state <= new_state;
@@ -64,40 +89,35 @@ begin
     process (clk)
     begin
         if rising_edge(clk) then
-            if reset = '1' then
+            if reset = '1' then -- when reset, go back to the initial state and set the output values
                 new_state <= idle;
-                sprite <= "00";
-                cnt_reset <= '1';
+                sprite    <= "00";
             else
                 case state is
                     when idle =>
-                        numstate <= "1111001"; --1
-                        sprite <= "00"; -- set sprite to idle
-                        cnt_reset <= '1';
+                        -- numstate <= "1111001"; --1
+                        sprite    <= "00"; -- set sprite to idle
                         if (controller_in = "00000100" or controller_in = "00001000" or controller_in = "00001001" or controller_in = "00001010" or controller_in = "00000110" or controller_in = "00000101" or controller_in = "00000111" or controller_in = "00001011") then -- make sure that going to duck is prioritised
                             new_state <= duck;
-                        elsif (controller_in = "00000001" or controller_in = "00000010") then
+                        elsif (controller_in = "00000001" or controller_in = "00000010") then -- go to the run animation only when left or right is pressed
                             new_state <= run_frame1;
                         else -- remain in idle whenever nothing is pressed
                             new_state <= idle;
                         end if;
-                    when duck => 
-                        numstate <= "0100100"; --2
-                        sprite <= "01"; -- set sprite to duck
-                        cnt_reset <= '1';
+                    when duck =>
+                        -- numstate <= "0100100"; --2
+                        sprite    <= "01"; -- set sprite to duck
                         if (controller_in = "00000000" or controller_in = "00000011") then -- back to idle when nothing is pressed
                             new_state <= idle;
                         elsif (controller_in = "00000001" or controller_in = "00000010") then -- go to the run animation only when left or right is pressed
-                            new_state <=  run_frame1;
+                            new_state <= run_frame1;
                         else
                             new_state <= duck;
                         end if;
                     when run_frame1 =>
-                        numstate <= "0110000"; --3
-                        sprite <= "10"; -- set sprite to run
-                        cnt_reset <= '0';
-                        if animation_clk = '1' then
-                            cnt_reset <= '1';
+                        -- numstate <= "0110000"; --3
+                        sprite    <= "10"; -- set sprite to run
+                        if cnt_reset = '1' then
                             new_state <= run_frame2;
                         elsif (controller_in = "00000000" or controller_in = "00000011") then -- back to idle when nothing is pressed
                             new_state <= idle;
@@ -107,11 +127,9 @@ begin
                             new_state <= run_frame1;
                         end if;
                     when run_frame2 =>
-                        numstate <= "0011001"; --4
-                        sprite <= "00"; -- set sprite to idle for animation purposes
-                        cnt_reset <= '0';
-                        if animation_clk = '1' then
-                            cnt_reset <= '1';
+                        -- numstate <= "0011001"; --4
+                        sprite    <= "00"; -- set sprite to idle for animation purposes
+                        if cnt_reset = '1' then
                             new_state <= run_frame1;
                         elsif (controller_in = "00000000" or controller_in = "00000011") then -- back to idle when nothing is pressed
                             new_state <= idle;
--- VGA/VGA_sprite_clk_gen.vhd
+++ VGA/VGA_sprite_clk_gen.vhd
@@ -8,7 +8,6 @@
 --! 
 --! 
 --! 
---! at this time obsolete, may become relevant later
 --------------------------------------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------------------------
 
@@ -16,33 +15,29 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity sprite_clk_gen is
+entity animation_clk_gen is
     port (
         clk       : in std_logic;
         reset     : in std_logic;
-        count     : in std_logic_vector (3 downto 0);
-        sprite_clk      : out std_logic; 
-        cnt_reset : out std_logic
+        count     : in std_logic_vector (4 downto 0);
+        cnt_reset : out std_logic  -- this also functions as the animation clock
     );
-end entity sprite_clk_gen;
+end entity animation_clk_gen;
 
-architecture behaviour of sprite_clk_gen is
-    signal uns_count : unsigned(3 downto 0);
+architecture behaviour of animation_clk_gen is
+    signal uns_count : unsigned(4 downto 0);
 begin
 
     process (clk, uns_count)
     begin
         if rising_edge(clk) then
             if reset = '1' then
-                sprite_clk      <= '1';
                 cnt_reset <= '1';
             elsif reset = '0' then
-                if uns_count    <= 5 then --sync pulse
-                    sprite_clk            <= '0';
-                    cnt_reset       <= '0';
-                else --reset counter
-                    sprite_clk      <= '1';
+                if uns_count = 9 then -- reset the counter when ready
                     cnt_reset <= '1';
+                else -- when not ready for next frame keep counting
+                    cnt_reset <= '0';
                 end if;
             end if;
         end if;

db8434626cb8094a9e50125c857cb4f0e7ca3264
Kevin <kevinvermaat@icloud.com>
2024-01-05 21:11:44 +0100

    updated the fsm to have control over the frame counter

--- VGA/VGA_char_animation_fsm.vhd
+++ VGA/VGA_char_animation_fsm.vhd
@@ -35,15 +35,6 @@ entity char_animation_fsm is
 end char_animation_fsm;
 architecture behaviour of char_animation_fsm is
 
-    component animation_clk_gen is
-        port (
-            clk       : in std_logic;
-            reset     : in std_logic;
-            count     : in std_logic_vector (4 downto 0);
-            cnt_reset : out std_logic -- this also functions as the animation clock
-        );
-    end component;
-
     component frame_cnt is
         port (
             clk : in std_logic;
@@ -55,7 +46,6 @@ architecture behaviour of char_animation_fsm is
     end component;
 
     signal cnt_reset     : std_logic;
-    signal sprite_clk : std_logic;
     signal frame_count : std_logic_vector(4 downto 0);
     type sprite_state is (
         idle, duck, run_frame1, run_frame2
@@ -63,14 +53,6 @@ architecture behaviour of char_animation_fsm is
     signal state, new_state : sprite_state;
 
 begin
-
-    clk_gen : animation_clk_gen port map (
-        clk => clk,
-        reset => reset,
-        count => frame_count,
-        cnt_reset => cnt_reset
-    );
-
     cnt : frame_cnt port map(
         clk    => clk,
         reset  => cnt_reset,
@@ -96,46 +78,60 @@ begin
                 case state is
                     when idle =>
                         -- numstate <= "1111001"; --1
+                        cnt_reset <= '1';
                         sprite    <= "00"; -- set sprite to idle
                         if (controller_in = "00000100" or controller_in = "00001000" or controller_in = "00001001" or controller_in = "00001010" or controller_in = "00000110" or controller_in = "00000101" or controller_in = "00000111" or controller_in = "00001011") then -- make sure that going to duck is prioritised
                             new_state <= duck;
                         elsif (controller_in = "00000001" or controller_in = "00000010") then -- go to the run animation only when left or right is pressed
+                            cnt_reset <= '0';
                             new_state <= run_frame1;
                         else -- remain in idle whenever nothing is pressed
                             new_state <= idle;
                         end if;
                     when duck =>
                         -- numstate <= "0100100"; --2
+                        cnt_reset <= '1';
                         sprite    <= "01"; -- set sprite to duck
                         if (controller_in = "00000000" or controller_in = "00000011") then -- back to idle when nothing is pressed
                             new_state <= idle;
                         elsif (controller_in = "00000001" or controller_in = "00000010") then -- go to the run animation only when left or right is pressed
+                            cnt_reset <= '0';
                             new_state <= run_frame1;
                         else
                             new_state <= duck;
                         end if;
                     when run_frame1 =>
                         -- numstate <= "0110000"; --3
+                        cnt_reset <= '0';
                         sprite    <= "10"; -- set sprite to run
-                        if cnt_reset = '1' then
+                        if unsigned(frame_count) >= 9 then
+                            cnt_reset <= '1';
                             new_state <= run_frame2;
                         elsif (controller_in = "00000000" or controller_in = "00000011") then -- back to idle when nothing is pressed
+                            cnt_reset <= '1';
                             new_state <= idle;
                         elsif (controller_in = "00000100" or controller_in = "00001000" or controller_in = "00001001" or controller_in = "00001010" or controller_in = "00000110" or controller_in = "00000101" or controller_in = "00000111" or controller_in = "00001011") then -- make sure that going to duck is prioritised
+                            cnt_reset <= '1';
                             new_state <= duck;
                         else
+                            cnt_reset <= '0';
                             new_state <= run_frame1;
                         end if;
                     when run_frame2 =>
                         -- numstate <= "0011001"; --4
+                        cnt_reset <= '0';
                         sprite    <= "00"; -- set sprite to idle for animation purposes
-                        if cnt_reset = '1' then
+                        if unsigned(frame_count) >= 9 then
+                            cnt_reset <= '1';
                             new_state <= run_frame1;
                         elsif (controller_in = "00000000" or controller_in = "00000011") then -- back to idle when nothing is pressed
+                            cnt_reset <= '1';
                             new_state <= idle;
                         elsif (controller_in = "00000100" or controller_in = "00001000" or controller_in = "00001001" or controller_in = "00001010" or controller_in = "00000110" or controller_in = "00000101" or controller_in = "00000111" or controller_in = "00001011") then -- make sure that going to duck is prioritised
+                            cnt_reset <= '1';
                             new_state <= duck;
                         else
+                            cnt_reset <= '0';
                             new_state <= run_frame2;
                         end if;
                     when others =>

b821830f66c31fa0a97254cfe91082988bf25339
Kevin Vermaat <kevinvermaat@icloud.com>
2024-01-09 15:25:57 +0100

    again more changes in the configurations

--- VGA/VGA_char_animation_fsm_cfg.vhd
+++ VGA/VGA_char_animation_fsm_cfg.vhd
@@ -1,5 +1,6 @@
 configuration char_animation_fsm_cfg of char_animation_fsm is
     for behaviour
         for all : frame_cnt use configuration work.frame_cnt_cfg; 
+        end for;
     end for;
 end configuration char_animation_fsm_cfg;

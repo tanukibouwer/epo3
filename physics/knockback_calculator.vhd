df803a8fdf68cbcc3e4ca35ae7ae81732a22391e
tanukibouwer <amara@qudical.nl>
2024-01-09 11:34:31 +0100

    Add additional y knockback

--- physics/knockback_calculator.vhd
+++ physics/knockback_calculator.vhd
@@ -13,9 +13,12 @@ entity p_knockback_calculator is
 end p_knockback_calculator;
 
 architecture behaviour of p_knockback_calculator is
-   constant balance_factor : std_logic_vector(4 downto 0) := "00100"; -- Signed
+   constant balance_factor_x : std_logic_vector(4 downto 0) := "00100"; -- Signed
+   constant balance_factor_y : std_logic_vector(4 downto 0) := "00100"; -- Signed
+   constant y_knockback_addition : std_logic_vector(7 downto 0) := "01000000"; -- Same format as knockback_y
    -- sorry :(
-   signal shifted_percentage : std_logic_vector(14 downto 0) := (others => '0');
+   signal shifted_percentage_x : std_logic_vector(14 downto 0) := (others => '0');
+   signal shifted_percentage_y : std_logic_vector(14 downto 0) := (others => '0');
    signal multiplied_vector_x : std_logic_vector(22 downto 0) := (others => '0');
    signal multiplied_vector_y : std_logic_vector(22 downto 0) := (others => '0');
    signal bitshifted_vector_x : std_logic_vector(13 downto 0) := (others => '0');
@@ -26,13 +29,14 @@ architecture behaviour of p_knockback_calculator is
    signal bitshifted_multiplied_bitshifted_vector_y : std_logic_vector(12 downto 0) := (others => '0');
 
 begin
-   shifted_percentage <= '0' & knockback_percentage & "000000";
-   multiplied_vector_x <= std_logic_vector(signed(knockback_x) * signed(shifted_percentage));
-   multiplied_vector_y <= std_logic_vector(signed(knockback_y) * signed(shifted_percentage));
+   shifted_percentage_x <= '0' & knockback_percentage & "000000";
+   shifted_percentage_y <= '0' & std_logic_vector(signed(knockback_percentage) + signed(y_knockback_addition)) & "000000";
+   multiplied_vector_x <= std_logic_vector(signed(knockback_x) * signed(shifted_percentage_x));
+   multiplied_vector_y <= std_logic_vector(signed(knockback_y) * signed(shifted_percentage_y));
    bitshifted_vector_x <= multiplied_vector_x(22 downto 9);
    bitshifted_vector_y <= multiplied_vector_y(22 downto 9);
-   multiplied_bitshifted_vector_x <= std_logic_vector(signed(bitshifted_vector_x) * signed(balance_factor));
-   multiplied_bitshifted_vector_y <= std_logic_vector(signed(bitshifted_vector_y) * signed(balance_factor));
+   multiplied_bitshifted_vector_x <= std_logic_vector(signed(bitshifted_vector_x) * signed(balance_factor_x));
+   multiplied_bitshifted_vector_y <= std_logic_vector(signed(bitshifted_vector_y) * signed(balance_factor_y));
    bitshifted_multiplied_bitshifted_vector_x <= multiplied_bitshifted_vector_x(18 downto 6);
    bitshifted_multiplied_bitshifted_vector_y <= multiplied_bitshifted_vector_y(18 downto 6);
    vout_x <= std_logic_vector(signed(vin_x) + signed(bitshifted_multiplied_bitshifted_vector_x(8 downto 0)));

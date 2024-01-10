e7d27695a78af4fe5324434065147bcd4d8287ac
Unrestricted-Hypercam-2 <tytusglab1@gmail.com>
2024-01-10 14:45:57 +0100

    forgot to add external initial value input in component declaration of m_toplevel

--- memory/m_toplevel.vhd
+++ memory/m_toplevel.vhd
@@ -83,6 +83,7 @@ architecture structural of memory is
 	port(
 		clk			: in std_logic;
 		reset		: in std_logic;
+		initial 	: in std_logic_vector(8 downto 0);
 		data_in 	: in std_logic_vector(8 downto 0);
 		data_out 	: out std_logic_vector(8 downto 0);
 		write 		: in std_logic);

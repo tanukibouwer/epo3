c263b4b8e35f9fcc77222dce6cdae1fea08af017
Unrestricted-Hypercam-2 <tytusglab1@gmail.com>
2024-01-10 14:38:43 +0100

    memory and toplevel changed to support double resolution

--- memory/m_ram9bit.vhd
+++ memory/m_ram9bit.vhd
@@ -6,6 +6,7 @@ entity ram_9b is -- 4 of these
 port(
 	clk			: in std_logic;
 	reset		: in std_logic;
+	initial		: in std_logic_vector(8 downto 0);
 	data_in 	: in std_logic_vector(8 downto 0);
 	data_out 	: out std_logic_vector(8 downto 0);
 	write 		: in std_logic);
@@ -20,7 +21,7 @@ begin
 	begin
 		if (rising_edge (clk)) then
 			if (reset = '1') then
-				mem(0) <= "000000000";
+				mem(0) <= initial;
 			else
 				if (write = '1') then 
 					mem(0) <= data_in;

23dad4d445925cef478096d19e3e4966c96df6b2
Kevin Vermaat <kevinvermaat@icloud.com>
2024-01-12 12:12:11 +0100

    upped the jump velocity constant

--- physics/jump_calculator.vhd
+++ physics/jump_calculator.vhd
@@ -11,7 +11,7 @@ end jump_calculator;
 
 architecture behaviour of jump_calculator is
 
-constant	jump_velocity	: std_logic_vector := "1111110111"; -- -9
+constant	jump_velocity	: std_logic_vector := "1111110011"; -- -9
 
 begin
 

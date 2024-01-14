21b4b9d6a275b41f5b7171856599fdd10ace4e83
Kevin <kevinvermaat@icloud.com>
2024-01-14 20:40:10 +0100

    made a small error in the cfg file

--- main/t_game_state_fsm_cfg.vhd
+++ main/t_game_state_fsm_cfg.vhd
@@ -1,6 +1,4 @@
 configuration game_state_fsm_cfg of game_state_fsm is
     for behaviour
-        for all : game_state_fsm use configuration work.game_state_fsm_cfg;
-        end for;
     end for;
 end configuration game_state_fsm_cfg;

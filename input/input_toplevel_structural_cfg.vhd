f73ed3ffe8dc15e256ca6420bda5d55010841926
Kevin Vermaat <kevinvermaat@icloud.com>
2024-01-16 14:09:27 +0100

    update the input cfg file

--- input/input_toplevel_structural_cfg.vhd
+++ input/input_toplevel_structural_cfg.vhd
@@ -8,7 +8,5 @@ end for;
 end for;
 		for all: input_register use configuration work.input_register_behavioural_cfg;
 end for;
-		for all: input_jump use configuration work.input_jump_behavioural_cfg;
-end for;
 	end for;
 end configuration input_toplevel_structural_cfg;

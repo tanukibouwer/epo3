9e508b5e181b0e5ea68cbc69e62d47c39bcf74bd
Parama Fawwaz <ParamaFawwaz@student.tudelft.nl>
2024-01-19 15:55:56 +0100

    Update Lopen_toplevel_tb.vhd

--- main/Report_tbs/Lopen_toplevel_tb.vhd
+++ main/Report_tbs/Lopen_toplevel_tb.vhd
@@ -48,7 +48,7 @@ architecture behaviour of Lopen_toplevel_tb is
            '1' after 20 ns when clk /= '1' else '0' after 20 ns;
     reset <= '1' after 0 ns,
              '0' after 80 ns;
-    p1_controller <= '1' after 0 ns, '0' after 9424 ns, '1' after 10311 ns, '0' after 20290 ns, '1' after 21066 ns, '0' after 31200 ns, '1' after 31666 ns, '0' after 42061 ns, '1' after 42615 ns, '0' after 52860 ns, '1' after 53483 ns;
+    p1_controller <= '1' after 0 ns, '0' after 129059 ns, '1' after 130072 ns;
     p2_controller <= '1' after 0 ns;
 
 
